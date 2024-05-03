acb68f338475e68133ca9dc2cc1b1be7de31aab4
Bryan Armando <mamanibryan1@gmail.com>
2024-05-02 22:59:28 -0500

    add type book

---
+++ src/types/book.ts
@@ -0,0 +1,10 @@
+export interface book {
+    title: string;
+    pages: string;
+    genre: string;
+    cover: string;
+    synopsis: string;
+    year: string;
+    ISBN: string;
+    author: string;
+}
