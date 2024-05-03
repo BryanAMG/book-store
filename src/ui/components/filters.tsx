aeadc4535b51799f2b9c90b2f2465eb44a4c5074
Bryan Armando <mamanibryan1@gmail.com>
2024-05-02 22:59:52 -0500

    add header and its components

--- src/ui/components/filters.tsx
+++ src/ui/components/filters.tsx
@@ -1,5 +1,9 @@
+import { Input } from "./Input";
+import { Categories } from "./categories";
+
 export function FiltersBooks() {
-    return <section >
-        <h2>Filtros</h2>
+    return <section className=" w-full flex flex-col items-center gap-2">
+        <Categories />
+        <Input placeholder="Busca por titulo " className="w-80 text-black" />
     </section>
 }
--- src/ui/components/header-layout.tsx
+++ src/ui/components/header-layout.tsx
@@ -1,11 +1,9 @@
-import { ReadListOpenButton } from "./icons";
+import { FiltersBooks } from "./filters";
+import { Header } from "./header";
 
 export function HeaderLayout() {
-    return <header className="w-full flex justify-between items-center py-3">
-        <h1 className="text-2xl italic">BOOK STORE</h1>
-        <button className="relative text-white bg-blue-700 rounded-lg p-2 hover:brightness-110  " aria-label="open-readlist">
-            <ReadListOpenButton />
-            <span className="absolute right-0 bottom-0 w-8 h-8 translate-x-1/2 translate-y-1/2 border-4 border-black  rounded-full bg-sky-300 text-base text-black  flex justify-center items-center">5</span>
-        </button>
-    </header>
+    return <div className="z-10 sticky top-0 w-full flex flex-col items-center bg-black">
+        <Header />
+        <FiltersBooks />
+    </div>
 }
