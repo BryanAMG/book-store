db37cdd0b83c3124e66f3b9b2ef7ddecffc1f718
Bryan Armando <mamanibryan1@gmail.com>
2024-05-02 22:58:47 -0500

    add grid book

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Image from "next/image";
+import books from '@/lib/books.json'
 
 export default function Home() {
+  const [example] = books.books
+  const {
+    cover,
+    title
+  } = example
   return (
-    <section className="">
-      listado de pelis
+    <section className="w-full grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-3 ">
+      <article className="group w-40 flex flex-col items-center ">
+        <picture className="relative overflow-hidden ">
+          <Image className=" w-full h-auto object-cover group-hover:brightness-50 transition duration-300" src={cover} alt={`image of ${title} book`} width={310} height={450} />
+          <div className="absolute inset-0 grid place-items-center ">
+            <div className="flex py-2 px-4 bg-black gap-6 cursor-pointer rounded-full translate-y-[350%] group-hover:translate-y-0  transition-transform">
+              <span className="w-8 h-8 flex justify-center items-center hover:text-blue-700">ver </span>
+              <span className="w-8 h-8 flex justify-center items-center hover:text-blue-700">add </span>
+            </div>
+          </div>
+        </picture>
+        <h3 className="mt-2 font-semibold text-center truncate">{title}</h3>
+      </article>
+
     </section>
   );
 }
