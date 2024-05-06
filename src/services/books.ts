057d7e1b755668f27901a4180685d43001beada7
Bryan Armando <mamanibryan1@gmail.com>
2024-05-06 00:13:01 -0500

    add data and functions about the books

--- src/services/books.ts
+++ src/services/books.ts
@@ -1,24 +1,48 @@
-import { books } from '@/lib/books.json'
-import type { book } from '@/types/book'
-export const getBooks = ({ genre = null, search = null }: { genre: string | null, search: string | null }) => {
-    if (!genre && !search) return books
-    return books.filter(book => {
-        return (
-            (genre === 'todos' || book.genre.toLocaleLowerCase() === genre?.toLocaleLowerCase()) &&
-            (search === null || book.title.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
-        )
+import booksData from '@/lib/books.json'
+import type { Book, Filters } from '@/types/book'
+
+const DELAY = 300
+const BOOKS_PER_PAGE = 8
+const { books } = booksData
+export const getBooks = ({ genre = "todos", search = "", page = 1 }: Filters): Promise<Book[]> => {
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+
+            const startIndex = (page - 1) * BOOKS_PER_PAGE
+            const endIndex = startIndex + BOOKS_PER_PAGE
+
+            if (genre === 'todos' && !search) resolve(!page ? books : books.slice(startIndex, endIndex))
+
+            const fitlerData = books.filter(book => {
+                return (
+                    (genre === 'todos' || book.genre.toLocaleLowerCase() === genre.toLocaleLowerCase()) &&
+                    (!search || book.title.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+                )
+            })
+            resolve(!page ? fitlerData : fitlerData.slice(startIndex, endIndex))
+        }, DELAY)
     })
+
+
 }
 
-export const getGenres = ({ filterBooks }: { filterBooks: book[] }) => {
+export const getGenres = (): Promise<string[]> => {
+    return new Promise(resolve => setTimeout(() => resolve(['todos'].concat(Array.from(new Set(books.map(book => book.genre))))), DELAY))
+}
+
+export const getBook = (ISBN: string): Promise<Book> => {
+    return new Promise(resolve => setTimeout(() => books.find(book => book.ISBN === ISBN), DELAY))
+}
+
+export const getGenresQuantity = ({ filterBooks }: { filterBooks: Book[] }) => {
     let genres: Record<string, number> = {}
     let todos = 0
     filterBooks.forEach(book => {
-        genres[book.title] ??= 0
-        genres[book.title] += 1
+        genres[book.genre] ??= 0
+        genres[book.genre] += 1
         todos += 1
     })
 
-    return Object.entries({ todos, ...genres })
-        .map(([genre, quantity]) => ({ genre, quantity }))
+    return { todos, ...genres }
 }
