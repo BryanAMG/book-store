7d8e8500290cd4d5a77886d64f5a768d6c933b94
Bryan Armando <mamanibryan1@gmail.com>
2024-05-06 00:11:19 -0500

    use components to refacotring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,36 @@
-import Image from "next/image";
-import books from '@/lib/books.json'
+import { Suspense } from "react";
+import { BookList } from "@/ui/components/book-list";
+import { BookInfo } from "@/ui/components/book-info-dialog";
+import { getBook } from "@/services/books";
+import { Book } from "@/types/book";
 
-export default function Home() {
-  const [example] = books.books
-  const {
-    cover,
-    title
-  } = example
+export default async function Home({
+  searchParams
+}: {
+  searchParams?: {
+    search?: string,
+    genre?: string,
+    isbn?: string,
+    page?: string
+  }
+}) {
+  const search = searchParams?.search ?? ''
+  const genre = searchParams?.genre ?? 'todos'
+  const page = Number(searchParams?.page ?? 1)
+
+  const isbn = searchParams?.isbn ?? ''
+  const book = await getBook(isbn)
+
   return (
-    <section className="w-full grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-3 ">
-      <article className="group w-40 flex flex-col items-center ">
-        <picture className="relative overflow-hidden ">
-          <Image className=" w-full h-auto object-cover group-hover:brightness-50 transition duration-300" src={cover} alt={`image of ${title} book`} width={310} height={450} />
-          <div className="absolute inset-0 grid place-items-center ">
-            <div className="flex py-2 px-4 bg-black gap-6 cursor-pointer rounded-full translate-y-[350%] group-hover:translate-y-0  transition-transform">
-              <span className="w-8 h-8 flex justify-center items-center hover:text-blue-700">ver </span>
-              <span className="w-8 h-8 flex justify-center items-center hover:text-blue-700">add </span>
-            </div>
-          </div>
-        </picture>
-        <h3 className="mt-2 font-semibold text-center truncate">{title}</h3>
-      </article>
-
+    <section className="flex w-full flex-col gap-4 items-center justify-center">
+      <Suspense key={search + genre + page} fallback={null}>
+        <BookList search={search} genre={genre} page={page} />
+      </Suspense>
+      {/* pagination */}
+      {
+        book && <BookInfo {...book} />
+      }
     </section>
+
   );
 }
