509ee7c0cad45c20b4742e04a08c1840f56ba972
Bryan Armando <mamanibryan1@gmail.com>
2024-05-08 21:18:58 -0500

    create components to favorite book item

---
+++ src/ui/components/read-list/reading-status.tsx
@@ -0,0 +1,6 @@
+
+export function ReadingStatus() {
+    return <label htmlFor="">
+        <input type=" " />
+    </label>
+}
---
+++ src/ui/components/read-list/stars.tsx
@@ -0,0 +1,6 @@
+
+export function Stars() {
+    return <div>
+        <button> + </button>
+    </div>
+}
