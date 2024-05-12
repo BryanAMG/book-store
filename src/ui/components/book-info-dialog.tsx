bd6e0ae4915106e2b4dced496da51597fd8acbeb
Bryan Armando <mamanibryan1@gmail.com>
2024-05-12 00:57:02 -0500

    fix props

--- src/ui/components/book-info-dialog.tsx
+++ src/ui/components/book-info-dialog.tsx
@@ -4,30 +4,31 @@ import { ButtonCloseModal } from "./button-close-modal"
 import { ButtonAddFavoriteBooks } from "./button-add-read-list"
 
 
-export function BookInfo({ ISBN, author, cover, genre, synopsis, title, year }: Book) {
+export function BookInfo(book: Book) {
+    const synopsisText = book.synopsis.length > 140 ? `${book.synopsis.split(" ", 40).join(" ")}...` : book.synopsis
     return <div className="absolute inset-0 z-30 grid place-items-center bg-black/80" >
         <article className="flex items-start justify-center bg-yellow-100 gap-3 p-3 text-black max-w-[680px] flex-col rounded-sm relative transition-colors md:flex-row md:gap-8 md:p-8 ">
             <ButtonCloseModal />
             <picture className="h-80 w-auto rounded-lg shadow-2xl mx-auto">
                 <Image
                     className=" w-full h-full object-cover "
-                    src={cover}
-                    alt={`image of ${title} book`}
+                    src={book.cover}
+                    alt={`image of ${book.title} book`}
                     width={310}
                     height={450} />
             </picture>
             <div className="flex-1 w-full  h-80 flex flex-col gap-4 ">
                 <header className="flex flex-col items-center justify-center gap-1 md:items-start">
-                    <h3 className="text-lg">{title}</h3>
-                    <h4 className="text-sm"> <i>{author} - {year}</i>  </h4>
-                    <span className="py-1.5 px-5 w-fit rounded-full bg-gray-600 text-sm text-white">{genre}</span>
+                    <h3 className="text-lg">{book.title}</h3>
+                    <h4 className="text-sm"> <i>{book.author} - {book.year}</i>  </h4>
+                    <span className="py-1.5 px-5 w-fit rounded-full bg-gray-600 text-sm text-white">{book.genre}</span>
                 </header>
                 <p>
-                    <span className="text-2xl">{synopsis[0]}</span>
-                    <span>{synopsis.substring(1)}</span>
+                    <span className="text-2xl">{book.synopsis[0]}</span>
+                    <span className="text-pretty">{synopsisText}</span>
                 </p>
                 <footer className="flex-1 flex flex-col justify-end">
-                    <ButtonAddFavoriteBooks ISBN={ISBN} large key={ISBN} >
+                    <ButtonAddFavoriteBooks book={book} large key={book.ISBN} >
                         <span>Añadir a la lista de lectura</span>
                     </ButtonAddFavoriteBooks>
                 </footer>
--- src/ui/components/book-item.tsx
+++ src/ui/components/book-item.tsx
@@ -4,21 +4,21 @@ import { ButtonOpenModal } from "./button-open-modal";
 import { ButtonAddFavoriteBooks } from "./button-add-read-list";
 import { BookInfo, BookToRead } from "./icons";
 
-export function BookItem({ title, cover, ISBN }: Book) {
+export function BookItem(book: Book) {
 
     return <article className="group mx-auto w-40 flex flex-col items-center overflow-hidden ">
         <picture className="relative overflow-hidden rounded-md shadow-lg ">
-            <Image className=" w-full h-[250px] object-cover group-hover:brightness-50 transition duration-500" src={cover} alt={`image of ${title} book`} width={310} height={450} />
+            <Image className=" w-full h-[250px] object-cover group-hover:brightness-50 transition duration-500" src={book.cover} alt={`image of ${book.title} book`} width={310} height={450} />
             <div className="absolute inset-0 grid place-items-center ">
                 <div className="flex py-1 px-3 bg-black gap-6 cursor-pointer rounded-full translate-y-[400%] duration-500 group-hover:translate-y-0  transition-transform">
-                    <ButtonOpenModal ISBN={ISBN} />
-                    <ButtonAddFavoriteBooks ISBN={ISBN}>
+                    <ButtonOpenModal ISBN={book.ISBN} />
+                    <ButtonAddFavoriteBooks book={book}>
                         <BookToRead />
                     </ButtonAddFavoriteBooks>
 
                 </div>
             </div>
         </picture>
-        <h3 className="w-full mt-2 font-semibold text-center truncate">{title}</h3>
+        <h3 className="w-full mt-2 font-semibold text-center truncate">{book.title}</h3>
     </article>
 }
