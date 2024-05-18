775f0d693c8ac1739322179cfafd5c9c4485043d
Bryan Armando <mamanibryan1@gmail.com>
2024-05-17 19:31:14 -0500

    add last suspense

--- src/ui/components/filters.tsx
+++ src/ui/components/filters.tsx
@@ -2,16 +2,13 @@
 import { GenreFilter } from "./genres";
 import { getGenres } from "@/services/books";
 import { InputFilter } from "./input-filter";
-import { Suspense } from "react";
 
 export async function FiltersBooks() {
 
     const allGenres: string[] = await getGenres()
     return <section className=" w-full flex flex-col items-center gap-4">
         <GenreFilter allGenres={allGenres} />
-        <Suspense>
-            <InputFilter placeholder="Buscar por titulo " />
-        </Suspense>
+        <InputFilter placeholder="Buscar por titulo " />
 
     </section>
 }
--- src/ui/components/header-layout.tsx
+++ src/ui/components/header-layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { FiltersBooks } from "./filters";
 import { Header } from "./header";
 
@@ -5,6 +6,9 @@ export function HeaderLayout() {
 
     return <div className="z-10 sticky top-0 w-full flex flex-col items-center bg-black py-2">
         <Header />
-        <FiltersBooks />
+        <Suspense>
+            <FiltersBooks />
+        </Suspense>
+
     </div>
 }
